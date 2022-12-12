a6df214735a0929688ed8020f5029b356f1f8345
William Bode <william.bode@gmx.de>
2022-12-12 10:56:47 +0100

    remove unused endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root 'bookings#index'
   namespace :api do
     namespace :v1 do
-      resources :bookings, only: %i[index create]
+      resources :bookings, only: %i[create]
       post 'bookings/available_slots', to: 'bookings#available_slots'
     end
   end
